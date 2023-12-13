c77ebefbada9b12dc1c526993620181e1b79fd69
usuario qualquer <luisgustavomacedo13@gmail.com>
2023-12-13 08:37:16 -0300

    feat: components

---
+++ builder/carProduction/components/GPSNavigator.ts
@@ -0,0 +1,11 @@
+export class GPSNavigator {
+  private route: string;
+
+  constructor (manualRoute: string) {
+    this.route = manualRoute ||  "221b, Baker Street, London  to Scotland Yard, 8-10 Broadway, London";
+  }
+
+  getRoute() {
+    return this.route;
+  }
+}
